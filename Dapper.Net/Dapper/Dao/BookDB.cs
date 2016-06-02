6fd4aa304f8d0bd55786d264b704a7b03432c2c6
13935 <hl13935@ly.com>
2016-06-02 11:24:40 +0800

    Dapper ORM demo 完成

---
+++ Dapper.Net/Dapper/Dao/BookDB.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper.Entity;
+
+namespace Dapper.Dao
+{
+    public class BookDB
+    {
+        public static IDbConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["DapperTest"].ConnectionString);
+
+        /// <summary>
+        /// insert
+        /// </summary>
+        /// <param name="BookName"></param>
+        public static void AddBook(string BookName)
+        {
+            Book book = new Book();
+            book.Name = "C#本质论";
+            string query = "INSERT INTO Book(Name)VALUES(@name)";
+            //对对象进行操作
+            conn.Execute(query, book);
+            //方法2直接赋值操作
+            //conn.Execute(query, new { name = "C#本质论" });
+        }
+
+        /// <summary>
+        /// update
+        /// </summary>
+        /// <param name="BookName"></param>
+        /// <param name="id"></param>
+        public static void UpdateBookById(string BookName, int Id)
+        {
+            Book book = new Book();
+            book.Name = BookName;
+            book.Id = Id;
+
+            string query = "UPDATE Book set Name=@Name where id=@Id";
+            conn.Execute(query, book);
+        }
+
+        /// <summary>
+        /// delete
+        /// </summary>
+        /// <param name="BookName"></param>
+        /// <param name="Id"></param>
+        public static void DeleteBookById(string BookName, int Id)
+        {
+            Book book = new Book();
+            book.Id = Id;
+
+            string query = "DELETE FROM Book WHERE id = @Id";
+            conn.Execute(query, book);
+            //conn.Execute(query, new { id = Id });  //方法2
+        }
+
+        /// <summary>
+        /// query
+        /// </summary>
+        /// <returns></returns>
+        public static List<Book> SelectBookList()
+        {
+            string query = "SELECT * FROM Book";
+            //无参数查询，返回列表，带参数查询和之前的参数赋值法相同。
+            return conn.Query<Book>(query).ToList();
+        }
+
+        /// <summary>
+        /// 返回单条信息
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public static Book SelectBookById(int id)
+        {
+            //返回单条信息
+            string query = "SELECT * FROM Book WHERE id = @id";
+            return conn.Query<Book>(query, new { id = id }).SingleOrDefault();
+        }
+
+        /// <summary>
+        /// 事务操作
+        /// </summary>
+        /// <param name="id"></param>
+        public static void TranDeleteBook(int id)
+        {
+            using (conn)
+            {
+                //开始事务
+                IDbTransaction transaction = conn.BeginTransaction();
+                try
+                {
+                    string query = "DELETE FROM Book WHERE id = @id";
+                    string query2 = "DELETE FORM BookReview WHERE BookId = @BookId";
+                    conn.Execute(query2, new { BookId = id }, transaction, null, null);
+                    conn.Execute(query, new { id = id }, transaction, null, null);
+                    //提交事务
+                    transaction.Commit();
+                }
+                catch (Exception ex)
+                {
+                    //出现异常，事务Rollback
+                    transaction.Rollback();
+                    throw new Exception(ex.Message);
+                }
+            }
+        }
+    }
+}
--- Dapper.Net/Dapper/Program.cs
+++ Dapper.Net/Dapper/Program.cs
@@ -7,11 +7,14 @@ using System.Text;
 using System.Threading.Tasks;
 using Dapper;
 using System.Data.SqlClient;
+using System.Configuration;
 
 namespace DapperDemo
 {
     class Program
     {
+        private static string sqlConnectionString =ConfigurationManager.ConnectionStrings["DapperTest"].ConnectionString;// "Data Source=10.14.86.186,1433;Initial Catalog=master;User ID=TCBaseTrain;Password=TCBaseTrain;MultipleActiveResultSets=True";
+
         static void Main(string[] args)
         {
         }
@@ -20,10 +23,10 @@ namespace DapperDemo
         /// 一对一映射
         /// </summary>
         /// <param name="sqlConnectionString"></param>
-        private static void OneToOne(string sqlConnectionString)
+        private static void OneToOne()
         {
             List<Customer> userList = new List<Customer>();
-            using (IDbConnection conn = GetSqlConnection(sqlConnectionString))
+            using (IDbConnection conn = GetSqlConnection())
             {
                 string sqlCommandText = @"SELECT c.UserId,c.Username AS UserName,
 c.PasswordHash AS [Password],c.Email,c.PhoneNumber,c.IsFirstTimeLogin,c.AccessFailedCount,
@@ -52,7 +55,141 @@ INNER JOIN CICRole r ON r.RoleId = cr.RoleId";
             }
         }
 
-        private static SqlConnection GetSqlConnection(string sqlConnectionString)
+        /// <summary>
+        /// 一对多映射
+        /// </summary>
+        /// <param name="sqlConnectionString"></param>
+        private static void OneToMany()
+        {
+            Console.WriteLine("One To Many");
+            List<User> userList = new List<User>();
+
+            using (IDbConnection connection = GetSqlConnection())
+            {
+
+                string sqlCommandText3 = @"SELECT c.UserId,
+       c.Username      AS UserName,
+       c.PasswordHash  AS [Password],
+       c.Email,
+       c.PhoneNumber,
+       c.IsFirstTimeLogin,
+       c.AccessFailedCount,
+       c.CreationDate,
+       c.IsActive,
+       r.RoleId,
+       r.RoleName
+FROM   dbo.CICUser c WITH(NOLOCK)
+       LEFT JOIN CICUserRole cr
+            ON  cr.UserId = c.UserId
+       LEFT JOIN CICRole r
+            ON  r.RoleId = cr.RoleId";
+
+                var lookUp = new Dictionary<int, User>();
+                userList = connection.Query<User, Role, User>(sqlCommandText3,
+                    (user, role) =>
+                    {
+                        User u;
+                        if (!lookUp.TryGetValue(user.UserId, out u))
+                        {
+                            lookUp.Add(user.UserId, u = user);
+                        }
+                        u.Role.Add(role);
+                        return user;
+                    }, null, null, true, "RoleId", null, null).ToList();
+                var result = lookUp.Values;
+            }
+
+            if (userList.Count > 0)
+            {
+                userList.ForEach((item) => Console.WriteLine("UserName:" + item.UserName +
+                                             "----Password:" + item.Password +
+                                             "-----Role:" + item.Role.First().RoleName +
+                                             "\n"));
+
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("No Data In UserList!");
+            }
+        }
+
+        /// <summary>
+        /// 插入实体
+        /// </summary>
+        private static void InsertObject()
+        {
+            string sqlCommandText = @"INSERT INTO CICUser(Username,PasswordHash,Email,PhoneNumber)VALUES(
+    @UserName,
+    @Password,
+    @Email,
+    @PhoneNumber
+)";
+            using (IDbConnection conn = GetSqlConnection())
+            {
+                User user = new User();
+                user.UserName = "Dapper";
+                user.Password = "654321";
+                user.Email = "Dapper@infosys.com";
+                user.PhoneNumber = "13795666243";
+                int result = conn.Execute(sqlCommandText, user);
+                if (result > 0)
+                {
+                    Console.WriteLine("Data have already inserted into DB!");
+                }
+                else
+                {
+                    Console.WriteLine("Insert Failed!");
+                }
+
+                Console.ReadLine();
+            }
+        }
+
+        /// <summary>
+        /// Execute StoredProcedure and map result to POCO
+        /// </summary>
+        /// <param name="sqlConnnectionString"></param>
+        private static void ExecuteStoredProcedure()
+        {
+            List<User> users = new List<User>();
+            using (IDbConnection cnn = GetSqlConnection())
+            {
+                users = cnn.Query<User>("dbo.p_getUsers",
+                                        new { UserId = 2 },
+                                        null,
+                                        true,
+                                        null,
+                                        CommandType.StoredProcedure).ToList();
+            }
+            if (users.Count > 0)
+            {
+                users.ForEach((user) => Console.WriteLine(user.UserName + "\n"));
+            }
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Execute StroedProcedure and get result from return value
+        /// </summary>
+        /// <param name="sqlConnnectionString"></param>
+        private static void ExecuteStoredProcedureWithParms(string sqlConnnectionString)
+        {
+            DynamicParameters p = new DynamicParameters();
+            p.Add("@UserName", "cooper");
+            p.Add("@Password", "123456");
+            p.Add("@LoginActionType", null, DbType.Int32, ParameterDirection.ReturnValue);
+            using (IDbConnection cnn = GetSqlConnection())
+            {
+                cnn.Execute("dbo.p_validateUser", p, null, null, CommandType.StoredProcedure);
+                int result = p.Get<int>("@LoginActionType");
+                Console.WriteLine(result);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static SqlConnection GetSqlConnection()
         {
             SqlConnection conn = new SqlConnection(sqlConnectionString);
             conn.Open();
